88625a6b5fab6c039b4323c92af3c16cb03130be
Pablo Cruz Ramos <A01138740@itesm.mx>
2024-01-27 00:11:10 -0600

    refactoring card component

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import ExperienceCard from "./ExperienceCard";
-
+import { CVInformation } from "../constants/cvInfo";
+import { CardProps } from "../interfaces";
 const Experience = () => {
- return <div className="pt-32"></div>;
+ return (
+  <ul>
+   {CVInformation.map((info: CardProps) => (
+    <ExperienceCard
+     period={info.period}
+     position={info.position}
+     description={info.description}
+     keywords={info.keywords}
+    />
+   ))}
+  </ul>
+ );
 };
 
 export default Experience;
--- src/constants/cvInfo.ts
+++ src/constants/cvInfo.ts
@@ -1,4 +1,36 @@
 import { CardProps } from "../interfaces";
 export const CVInformation: CardProps[] = [
- { period: "", position: "", description: "", keywords: ["", ""] },
+ {
+  period: "OCT 2023 - PRESENT",
+  position: "Junior Software Engineer",
+  description: "falta poner una descripcion",
+  keywords: [
+   "React",
+   "Redux",
+   "TypeScript",
+   "HTML & CSS",
+   "AWS",
+   "Unit Testing",
+  ],
+ },
+ {
+  period: "AUG 2021 - JUNE 2022",
+  position: "Software Engineer Intern",
+  description: "falta poner una descripcion tambien bro",
+  keywords: [
+   "Angular",
+   "TypeScript",
+   "Pug",
+   "NodeJS",
+   "SpringBoot",
+   "Java",
+   "NGX",
+  ],
+ },
+ {
+  period: "FEB 2021 - MAY 2021",
+  position: "Backend Developer",
+  description: "poner lo que hiciste",
+  keywords: ["Ruby", "Ruby on Rails", "Firebase", "RSpec"],
+ },
 ];
