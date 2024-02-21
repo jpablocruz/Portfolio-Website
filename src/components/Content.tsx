2a0ff301e4492c0bccdbd09155c322a2793388c3
Pablo Cruz Ramos <A01138740@itesm.mx>
2024-02-20 20:12:16 -0600

    finished

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -15,8 +15,14 @@ const Content = () => {
     Coded in TypeScript using <b className="text-blue">React</b> and{" "}
     <b className="text-blue">Tailwind</b>, deployed and hosted with{" "}
     <b className="text-blue">Github Pages</b>. This page was heavily inspired by
-    Brittany Chiang´s Web Portfolio. If you like it you can check the repo. Dont
-    forget to give it a star <b className="text-white">★</b>.
+    Brittany Chiang´s Web Portfolio. If you like it you can check the{" "}
+    <a
+     href="https://github.com/jpablocruz/Portfolio-Website"
+     className="text-blue"
+    >
+     repo
+    </a>
+    . Dont forget to give it a star <b className="text-white">★</b>.
    </div>
   </div>
  );
